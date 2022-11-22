8c1656384cde11f4cadaca7b85a237a1414a4d2a
Jeffrey Cheung <jeffreycheung_@live.com>
2022-11-22 14:13:25 -0800

    created simple timer function

---
+++ timer1.js
@@ -0,0 +1,9 @@
+let array = process.argv.slice(2).filter(Number).sort((a, b) => a - b);
+
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > 0) {
+    setTimeout(() => {
+      process.stdout.write('\x07');
+    }, array[i] * 1000);
+  }
+}
